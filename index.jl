eedf4ea9dbfab8936af2757d20a25d10a855d871
Patrick Moehrke <rev0cation2025@outlook.com>
2025-02-03 23:48:12 +0200

    Update index.jl

--- index.jl
+++ index.jl
@@ -31,8 +31,11 @@ function LSTM1(in_dims, hidden_dims, out_dims)
     #end
 	return Chain(
 		Recurrence(GRUCell(in_dims => hidden_dims); return_sequence=true),
-		Recurrence(GRUCell(hidden_dims => hidden_dims); return_sequence=false),
-		Dense(hidden_dims => out_dims, exp)
+		Recurrence(GRUCell(hidden_dims => hidden_dims); return_sequence=true), # T=10
+		Dense(10 => out_dims, exp)
+#Dense(hidden_dims => out_dims, exp)
+
+
 	)
 end
 
