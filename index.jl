9aed6192026db72b4e1c1ed0f8d10021ca1a2b14
Patrick Moehrke <rev0cation2025@outlook.com>
2025-02-03 22:50:39 +0200

    Update index.jl

--- index.jl
+++ index.jl
@@ -172,8 +172,8 @@ end
 
 # ╔═╡ bb6602f4-927e-473e-b8da-957395ed7617
 begin
-plot(100:n_epochs, train_losses[100:end], xlab="Epochs", ylab="MSE", ylim=(0.0, 0.30), label="Training")
-plot!(100:n_epochs, valid_losses[100:end], label="Validation")
+plot(10:n_epochs, train_losses[10:end], xlab="Epochs", ylab="MSE", ylim=(0.0, 0.30), label="Training")
+plot!(10:n_epochs, valid_losses[10:end], label="Validation")
 end
 
 # ╔═╡ bbb80aa6-096c-4fae-bc1d-2e2f5ba28b2d
@@ -196,14 +196,14 @@ mean((exp.(-y_train) .- exp.(y_pred_train)) .^ 2)
 
 # ╔═╡ c59ed9df-f944-4ee6-881e-2986dc8b1d3d
 begin
-	plot(1:97, y_pred_valid, label="Predicted", width=2, title="Validation Set")
-	scatter!(1:97, -y_valid, label="Observed")
+	plot(1:97, vec(y_pred_valid), label="Predicted", width=2, title="Validation Set")
+	scatter!(1:97, -vec(y_valid'), label="Observed")
 end
 
 # ╔═╡ 13601f30-29d5-40f3-a8c2-18b8a25a4070
 begin
-	plot(1:97, y_pred_train, label="Predicted", width=2, title="Training Set")
-	scatter!(1:97, -y_train, label="Observed")
+	plot(1:97, vec(y_pred_train), label="Predicted", width=2, title="Training Set")
+	scatter!(1:97, -vec(y_train'), label="Observed")
 end
 
 # ╔═╡ 78133d49-6e4d-4506-9e0b-81cd058048c6
