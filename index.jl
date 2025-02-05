1d27d50013c9754695a8087d461cf5a30a0a8365
Patrick Moehrke <rev0cation2025@outlook.com>
2025-02-05 12:22:57 +0200

    Generalise to handle FNNs and any country

--- index.jl
+++ index.jl
@@ -5,174 +5,283 @@ using Markdown
 using InteractiveUtils
 
 # ╔═╡ 6264e41e-e179-11ef-19c1-f135e97db7cc
+# ╠═╡ show_logs = false
 begin
-	cd(".")
 	using Pkg
-	# Not registered
+	cd(".")
+	Pkg.activate(".")
 	Pkg.add(url="https://github.com/patrickm663/HMD.jl")
-	Pkg.instantiate()
-	Pkg.add(["CSV", "DataFrames", "Lux", "ADTypes", "Zygote", "Optimisers", "Plots", "ComponentArrays", "MLUtils"])
+	Pkg.add(["CSV", "DataFrames", "Lux", "ADTypes", "Zygote", "Optimisers", "Plots", "ComponentArrays"])
 end
 
 # ╔═╡ ef51c95e-d5ad-455a-9631-094823b695bb
-using ADTypes, Lux, Optimisers, Printf, Random, CSV, Plots, DataFrames, ComponentArrays, MLUtils, HMD, Zygote, Statistics
+using ADTypes, Lux, Optimisers, Printf, Random, CSV, Plots, DataFrames, ComponentArrays, HMD, Zygote, Statistics
 
-# ╔═╡ 93779239-cd66-4be2-b70f-c7872a29a29f
-function LSTM1(in_dims, hidden_dims, out_dims)
-    #lstm_cell = LSTMCell(in_dims => hidden_dims)
-    #regressor = Dense(hidden_dims => out_dims, exp)
-    #return @compact(; lstm_cell, regressor) do x::AbstractArray{T, 3} where {T}
-    #    x_init, x_rest = Iterators.peel(LuxOps.eachslice(x, Val(2)))
-    #    y, carry = lstm_cell(x_init)
-    #    for x in x_rest
-    #        y, carry = lstm_cell((x, carry))
-    #    end
-    #    @return vec(regressor(y))
-    #end
-	return Chain(
-		Recurrence(GRUCell(in_dims => hidden_dims); return_sequence=true),
-		Recurrence(GRUCell(hidden_dims => hidden_dims); return_sequence=false),
-		#x -> stack(x; dims=2),
-		Dense(hidden_dims => out_dims, exp)
-	)
+# ╔═╡ 50b3b576-d941-4609-8469-6de51cfa1545
+begin
+	start_year = 1990
+	end_year = 2001
+	τ₀ = 3
+	τ₁ = 8
+	T = 10
+	NN_depth = 4
+	cell = GRUCell #LSTMCell
+	act = tanh
+	list_of_countries = HMD.get_countries()
+	country = list_of_countries["Switzerland"]
+	lr = 0.001
+	opt = NAdam(lr)
+	model_type = "LSTM"
+	# Hard-code
+	if model_type == "NN"
+		τ₀ = 2
+	end
 end
 
-# ╔═╡ fea5ab5a-847b-4c16-a318-2993b6f3662f
-LSTM1(3, 5, 1)
+# ╔═╡ a0132c58-f427-4c88-ba45-bd8b9d9f98d4
+list_of_countries
 
-# ╔═╡ 6e4c5b3e-5daf-47db-b0ef-df1a69d249be
-rng = Xoshiro(222)
+# ╔═╡ 93779239-cd66-4be2-b70f-c7872a29a29f
+function LSTM(in_dims, hidden_dims, out_dims; depth=1, cell=LSTMCell)
+	@assert depth > 0
+	@assert cell ∈ [LSTMCell, GRUCell, RNNCell]
+	if depth == 1
+		return Chain(
+			Recurrence(cell(in_dims => hidden_dims); return_sequence=false),
+			Dense(hidden_dims => out_dims, exp)
+		)
+	elseif depth == 2
+		return Chain(
+			Recurrence(cell(in_dims => hidden_dims); return_sequence=true),
+			Recurrence(cell(hidden_dims => hidden_dims); return_sequence=false),
+			Dense(hidden_dims => out_dims, exp)
+		)
+	elseif depth > 2
+		return Chain(
+			Recurrence(cell(in_dims => hidden_dims); return_sequence=true),
+			[Recurrence(cell(hidden_dims => hidden_dims); return_sequence=true) for _ in 1:(depth-2)],
+			Recurrence(cell(hidden_dims => hidden_dims); return_sequence=false),
+			Dense(hidden_dims => out_dims, exp)
+		)
+	end
+end
 
-# ╔═╡ e250694d-4d5b-405d-846b-033088335737
-md"""
-```
-library ( keras )
-T
-<- 10
-tau0 <- 3
-tau1 <- 5
-# length of time series x_1 ,... , x_T
-# dimension inputs x_t
-# dimension of the neurons z_t ^(1) in the first RNN layer
-Input <- layer_input ( shape = c (T , tau0 ) , dtype = ’ float32 ’ , name = ’ Input ’)
-Output = Input % >%
-layer_lstm ( units = tau1 , activation = ’ tanh ’ , r e c u r r e n t _ a c t i v a t i o n = ’ tanh ’ , name = ’ LSTM1 ’) % >%
-layer_dense ( units =1 , activation = k_exp , name =" Output ")
-model <- keras_model ( inputs = list ( Input ) , outputs = c ( Output ))
-model % >% compile ( loss = ’ mean_squared_error ’ , optimizer = ’ nadam ’)
-summary ( model )
-```
-"""
-
-# ╔═╡ 9a0a2da5-53ed-46c2-8eb0-131d3a11f2c2
-md"""
-Each rate is neighboured by the previous and sequential rate (age-wise). The target is the mortality rate per age in the year 2 000.
-"""
-
-# ╔═╡ a5e6a3ca-9c2b-4064-8e9e-0ea2e91d7aed
-md"""
-```R
-mort_rates <- all_mort [ which ( al l _m or t$ G en de r ==" Female ") , c (" Year " , " Age " , " logmx ")]
-mort_rates <- dcast ( mort_rates , Year ~ Age , value . var =" logmx ")
-T0 <- 10
-tau0 <- 3
-# lookback period
-# dimension of x_t ( should be odd for our application )
-toy_rates <- as . matrix ( mort_rates [ which ( m or t_ r at es $ Ye ar % in % c (2001 - T0 -1):2001)) ,])
-# note that the first column in toy_rates is the " Year "
-xt <- array ( NA , c (2 , ncol ( toy_rates ) - tau0 , T0 , tau0 ))
-YT <- array ( NA , c (2 , ncol ( toy_rates ) - tau0 ))
-for ( i in 1:2){ for ( a0 in 1:( ncol ( toy_rates ) - tau0 )){
-xt [i , a0 , ,] <- toy_rates [ c ( i :( T0 +i -1)) , c (( a0 +1):( a0 + tau0 ))]
-YT [i , a0 ] <- toy_rates [ T0 +i , a0 +1+( tau0 -1)/2]
-}}
-```
-"""
+# ╔═╡ 608363b0-5f75-45c8-8970-5170489a5eeb
+function FNN(in_dims, hidden_dims, out_dims; depth=2, act=tanh, outer_act=exp)
+	@assert depth > 0
+	if depth == 1
+		return Chain(
+			Dense(in_dims => out_dims, outer_act)
+		)
+	elseif depth == 2
+		return Chain(
+			Dense(in_dims => hidden_dims, act),
+			Dense(hidden_dims => out_dims, outer_act)
+		)
+	elseif depth > 2
+		return Chain(
+			Dense(in_dims => hidden_dims, act),
+			[Dense(hidden_dims => hidden_dims, act) for _ in 1:(depth-2)],
+			Dense(hidden_dims => out_dims, outer_act)
+		)
+	end
+end
 
 # ╔═╡ 59eed207-e138-44be-a241-d4cbfde0f38c
-function get_data(; T=10, τ₀=3)
+function get_data(country; T=10, τ₀=3, start_year=1990, end_year=2001, LSTM_format=true, min_max_scale=true)
 	# Apply 'Toy example' data pre-processing
 	
     # Load from CSV
-    data = CSV.read("data/CHE_Mx_1x1.csv", DataFrame)
+    data = CSV.read("data/$(country)_Mx_1x1.csv", DataFrame)
 
-	# Split out females, aged 0-98, years 1990-2001
-	data = data[(data.Age .≤ 98) .&& (1990 .≤ data.Year .≤ 2001), [:Year, :Age, :Female]]
+	# Split out females, aged 0-98, years start_year-end_year
+	start_year = max(start_year, minimum(data.Year))
+	end_year = min(end_year, maximum(data.Year))
 
-	# Group age x year and drop the age column (1)
-	data = HMD.transform(data, :Female)[:, 2:end]
-	# Add for numerical stability
-	data .= data .+ 1e-9
+	@show start_year, end_year
 	
-	 # Convert to Matrix, transpose, take logs, and add extra column to match R code
-	data = hcat(1990:2001, log.(Matrix(data)'))
-
-	# 1 = train, 2 = validation
-	X_ = zeros(2, τ₀, T, size(data)[2]-τ₀) # 3, 10, :
-	y_ = zeros(2, size(data)[2]-τ₀)
-	for i in 1:2
-		for j in 1:(size(data)[2]-τ₀)
-			X_[i, :, :, j] = data[i:(T+i-1), ((j+1):(j+τ₀))]'
-			y_[i, j] = data[T+i, Int(j+1+(τ₀ - 1)/2)]'
-		end
-	end
-
-	# Min-max scale inputs to -1 - 1
+	@assert start_year < end_year
+	@assert end_year - start_year ≥ T
 	
-	X_min, X_max = (minimum(X_), maximum(X_))
+	data = data[(data.Age .≤ 98) .&& (start_year .≤ data.Year .≤ end_year), [:Year, :Age, :Female]]
 
 	min_max(x, xmin, xmax) = 2*(x - xmin) / (xmax - xmin) - 1
-	X_train = min_max.(X_[1, :, :, :], X_min, X_max) |> f32
-	X_valid = min_max.(X_[2, :, :, :], X_min, X_max) |> f32
+	standardise(x, x_mu, x_sigma) = (x - x_mu) / x_sigma
 
-	# Negative log mortality so outputs are positive
-	y_train = -y_[1, :] |> f32
-	y_valid = -y_[2, :] |> f32
+	if LSTM_format == true
+	
+		# Group age x year and drop the age column (1)
+		data = HMD.transform(data, :Female)[:, 2:end]
+	
+		# Get average mortality per age
+		avg_mortality = mean(Matrix(data), dims=2)
+	
+		# Change missing data to the mean
+		for i ∈ 1:size(data)[1]
+			for j ∈ 1:size(data)[2]
+				if data[i, j] == 0.0
+					if avg_mortality[i, 1] == 0
+						data[i, j] = avg_mortality[i+1, 1]
+					else
+						data[i, j] = avg_mortality[i, 1]
+					end
+				end
+			end
+		end
+		
+		 # Convert to Matrix, transpose, take logs, and add extra column to match R code
+		data = hcat(start_year:end_year, log.(Matrix(data)'))
+	
+		# 1 = train, 2 = validation
+		X_ = zeros(2, τ₀, T, size(data)[2]-τ₀) # 3, 10, :
+		y_ = zeros(2, size(data)[2]-τ₀)
+		for i in 1:2
+			for j in 1:(size(data)[2]-τ₀)
+				X_[i, :, :, j] = data[i:(T+i-1), ((j+1):(j+τ₀))]'
+				y_[i, j] = data[T+i, Int(j+1+(τ₀ - 1)/2)]'
+			end
+		end
 
-	return X_train, reshape(y_train, 1, :), X_valid, reshape(y_valid, 1, :), (X_min, X_max)
+		if min_max_scale == true
+			# Min-max scale inputs to -1 - 1
+			X_min, X_max = (minimum(X_), maximum(X_))
+		
+			X_train = min_max.(X_[1, :, :, :], X_min, X_max) |> f32
+			X_valid = min_max.(X_[2, :, :, :], X_min, X_max) |> f32
+		else
+			# Z-transform
+			X_mu, X_sigma = (mean(X_), std(X_))
+		
+			X_train = standardise.(X_[1, :, :, :], X_mu, X_sigma) |> f32
+			X_valid = standardise.(X_[2, :, :, :], X_mu, X_sigma) |> f32
+		end
+	
+		# Negative log mortality so outputs are positive
+		y_train = -y_[1, :] |> f32
+		y_valid = -y_[2, :] |> f32
+	
+		return X_train, reshape(y_train, 1, :), X_valid, reshape(y_valid, 1, :)
+		
+	else
+		# Drop missing from training set and validation sets
+		# But retain it for a final X test set
+		data = data[data.Female .> 0, :]
+		
+		train = data[data.Year .< 2001, :]
+		valid = data[data.Year .≥ 2001, :]
+
+		train = Matrix(train) |> f32
+		valid = Matrix(valid) |> f32
+
+		# Recreate the validation set but using all ages to test interpolation
+		X_test = hcat(repeat([2001.0], 99), 0:98) |> f32
+
+		X_train = train[:, 1:2]
+		X_valid = valid[:, 1:2]
+		y_train = -log.(train[:, end])
+		y_valid = -log.(valid[:, end])
+
+		validation_ages = X_valid[:, 2]
+
+		if min_max_scale == true
+			year_min, year_max = extrema(X_train[:, 1])
+			age_min, age_max = extrema(X_train[:, 2])
+	
+			X_train[:, 1] .= min_max.(X_train[:, 1], year_min, year_max)
+			X_valid[:, 1] .= min_max.(X_valid[:, 1], year_min, year_max)
+			X_test[:, 1] .= min_max.(X_test[:, 1], year_min, year_max)
+	
+			X_train[:, 2] .= min_max.(X_train[:, 2], age_min, age_max)
+			X_valid[:, 2] .= min_max.(X_valid[:, 2], age_min, age_max)
+			X_test[:, 2] .= min_max.(X_test[:, 2], age_min, age_max)
+		else
+			year_mu, year_sigma = (mean(X_train[:, 1]), std(X_train[:, 1]))
+			age_mu, age_sigma = (mean(X_train[:, 2]), std(X_train[:, 2]))
+	
+			X_train[:, 1] .= standardise.(X_train[:, 1], year_mu, year_sigma)
+			X_valid[:, 1] .= standardise.(X_valid[:, 1], year_mu, year_sigma)
+			X_test[:, 1] .= standardise.(X_test[:, 1], year_mu, year_sigma)
+	
+			X_train[:, 2] .= standardise.(X_train[:, 2], age_mu, age_sigma)
+			X_valid[:, 2] .= standardise.(X_valid[:, 2], age_mu, age_sigma)
+			X_test[:, 2] .= standardise.(X_test[:, 2], age_mu, age_sigma)
+		end
+
+		return X_train', reshape(y_train, 1, :), X_valid', reshape(y_valid, 1, :), X_test', validation_ages
+		
+	end
+		
 end
 
 # ╔═╡ 7b5558e7-30c8-4069-9175-6dd79d27c8f5
-X_train, y_train, X_valid, y_valid, min_max_scale = get_data()
+if model_type == "NN"
+	X_train, y_train, X_valid, y_valid, X_test, X_valid_ages = get_data(country; T=T, τ₀=τ₀, LSTM_format=false,  min_max_scale=false, start_year=start_year)
+else
+	X_train, y_train, X_valid, y_valid = get_data(country; T=T, τ₀=τ₀, LSTM_format=true,  min_max_scale=false, start_year=start_year)
+end
 
 # ╔═╡ 564d77df-85c1-406a-8964-3b14fca6328c
-function main(tstate::Training.TrainState, vjp, data, epochs)
+function main(tstate, vjp, data, epochs; early_stopping=true)
 	loss_function = MSELoss()
 	train_losses = []
 	valid_losses = []
+	
     for epoch in 1:epochs
         _, train_loss, _, tstate = Training.single_train_step!(vjp, loss_function, data, tstate)
 		push!(train_losses, train_loss)
 		valid_pred = (Lux.apply(tstate.model, X_valid, tstate.parameters, tstate.states))[1]
-valid_loss = mean((y_valid .- valid_pred) .^ 2)
-push!(valid_losses, valid_loss)
-
+		valid_loss = mean((y_valid .- valid_pred) .^ 2)
+		push!(valid_losses, valid_loss)
 
         if epoch % 50 == 1 || epoch == epochs
             @printf "Epoch: %3d \t Train Loss: %.5g \t Valid Loss: %.5g \n" epoch train_loss valid_loss
-        end
+		end
+
+		# Custom early-stopping
+		if early_stopping == true
+			if epoch > 150 && train_losses[end] < 0.10
+				if valid_losses[end] > valid_losses[end-10]
+					@printf "Early Stopping at Epoch: %3d \t" epoch
+					return tstate, train_losses, valid_losses
+				end
+	        end
+		end
     end
+	
     return tstate, train_losses, valid_losses
 end
 
 # ╔═╡ b7d6736b-776e-49d1-ae18-4a64b07a1a24
 begin
-	model = LSTM1(3, 5, 1)
-	ps, st = Lux.setup(rng, model)
+	if model_type == "NN"
+		model = FNN(τ₀, τ₁, 1; depth=NN_depth, act=act, outer_act=exp)
+	else
+		model = LSTM(τ₀, τ₁, 1; depth=NN_depth, cell=cell)
+	end
+	
+	ps, st = Lux.setup(Xoshiro(12345), model)
 
-	opt = Adam(0.01f0)
 	tstate = Training.TrainState(model, ps, st, opt)
 	
 	ad_rule = AutoZygote()
 
-	n_epochs = 1_000
-	@time tstate, train_losses, valid_losses = main(tstate, ad_rule, (X_train, y_train), n_epochs)
+	n_epochs = 500 # Max epochs
+	@time tstate, train_losses, valid_losses = main(tstate, ad_rule, (X_train, y_train), n_epochs; early_stopping=true)
 end
+
+# ╔═╡ 096a8a27-ddad-4534-94e4-71f755d5f561
+model
 
 # ╔═╡ bb6602f4-927e-473e-b8da-957395ed7617
 begin
-plot(10:n_epochs, train_losses[10:end], xlab="Epochs", ylab="MSE", ylim=(0.0, 0.30), label="Training")
-plot!(10:n_epochs, valid_losses[10:end], label="Validation")
+	if model_type == "NN"
+		plot(title="Losses: $(country)\n τ₁=$τ₁, act=$act, FNN, depth=$NN_depth", xlab="Epochs", ylab="MSE", ylim=(0.0, max(0.21, 1.5*minimum(valid_losses))))
+	else
+		plot(title="Losses: $(country)\n τ₀=$τ₀, τ₁=$τ₁, T=$T, cell=$cell, depth=$NN_depth", xlab="Epochs", ylab="MSE", ylim=(0.0, max(0.21, 1.5*minimum(valid_losses))))
+	end
+	scatter!(1:length(train_losses), train_losses, label="Training")
+	scatter!(1:length(valid_losses), valid_losses, label="Validation")
+	hline!([0.1], col=:black, label=false)
 end
 
 # ╔═╡ bbb80aa6-096c-4fae-bc1d-2e2f5ba28b2d
@@ -182,10 +291,10 @@ function predict(train_state, Xs)
 end
 
 # ╔═╡ 828445ab-f12c-4702-a008-9cf7091c084a
-y_pred_valid = predict(tstate, X_valid)
+y_pred_valid = predict(tstate, X_valid);
 
 # ╔═╡ f1f84de0-488a-4bad-a2a4-64965d493dc7
-y_pred_train = predict(tstate, X_train)
+y_pred_train = predict(tstate, X_train);
 
 # ╔═╡ 7e8e62f5-28a1-4153-892a-fc8988130f4b
 mean((exp.(-y_valid) .- exp.(y_pred_valid)) .^ 2)
@@ -195,14 +304,18 @@ mean((exp.(-y_train) .- exp.(y_pred_train)) .^ 2)
 
 # ╔═╡ c59ed9df-f944-4ee6-881e-2986dc8b1d3d
 begin
-	plot(1:97, vec(y_pred_valid), label="Predicted", width=2, title="Validation Set")
-	scatter!(1:97, -vec(y_valid'), label="Observed")
-end
-
-# ╔═╡ 13601f30-29d5-40f3-a8c2-18b8a25a4070
-begin
-	plot(1:97, vec(y_pred_train), label="Predicted", width=2, title="Training Set")
-	scatter!(1:97, -vec(y_train'), label="Observed")
+	if model_type == "NN"
+		y_pred_test = predict(tstate, X_test)
+		start_age = 0
+		end_age = 98
+		plot(start_age:end_age, vec(y_pred_test), label="Predicted", width=2, title="Validation Set ($end_year): $(country)\n τ₁=$τ₁, act=$act, FNN, depth=$NN_depth", xlab="Age", ylab="log μ", legend=:bottomright)
+		scatter!(X_valid_ages, vec(-y_valid'), label="Observed")
+	else
+		start_age = τ₀ - 2
+		end_age = 97
+		plot(start_age:end_age, vec(y_pred_valid), label="Predicted", width=2, title="Validation Set ($end_year): $(country)\n τ₀=$τ₀, τ₁=$τ₁, T=$T, cell=$cell, depth=$NN_depth", xlab="Age", ylab="log μ", legend=:bottomright)
+		scatter!(start_age:end_age, vec(-y_valid'), label="Observed")
+	end
 end
 
 # ╔═╡ 78133d49-6e4d-4506-9e0b-81cd058048c6
@@ -214,16 +327,15 @@ end
 # ╔═╡ Cell order:
 # ╠═6264e41e-e179-11ef-19c1-f135e97db7cc
 # ╠═ef51c95e-d5ad-455a-9631-094823b695bb
-# ╠═fea5ab5a-847b-4c16-a318-2993b6f3662f
+# ╠═50b3b576-d941-4609-8469-6de51cfa1545
+# ╠═a0132c58-f427-4c88-ba45-bd8b9d9f98d4
 # ╠═93779239-cd66-4be2-b70f-c7872a29a29f
-# ╠═6e4c5b3e-5daf-47db-b0ef-df1a69d249be
-# ╟─e250694d-4d5b-405d-846b-033088335737
-# ╟─9a0a2da5-53ed-46c2-8eb0-131d3a11f2c2
-# ╟─a5e6a3ca-9c2b-4064-8e9e-0ea2e91d7aed
+# ╠═608363b0-5f75-45c8-8970-5170489a5eeb
 # ╠═59eed207-e138-44be-a241-d4cbfde0f38c
 # ╠═7b5558e7-30c8-4069-9175-6dd79d27c8f5
 # ╠═564d77df-85c1-406a-8964-3b14fca6328c
 # ╠═b7d6736b-776e-49d1-ae18-4a64b07a1a24
+# ╠═096a8a27-ddad-4534-94e4-71f755d5f561
 # ╠═bb6602f4-927e-473e-b8da-957395ed7617
 # ╠═bbb80aa6-096c-4fae-bc1d-2e2f5ba28b2d
 # ╠═828445ab-f12c-4702-a008-9cf7091c084a
@@ -231,6 +343,5 @@ end
 # ╠═7e8e62f5-28a1-4153-892a-fc8988130f4b
 # ╠═c8bca677-24d5-4bcc-b881-e0f43f208ca9
 # ╠═c59ed9df-f944-4ee6-881e-2986dc8b1d3d
-# ╠═13601f30-29d5-40f3-a8c2-18b8a25a4070
 # ╠═78133d49-6e4d-4506-9e0b-81cd058048c6
 # ╠═7844b633-e043-489a-9627-3f87e13d296d
